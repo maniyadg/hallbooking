463482f1272945c64f704be2831163fadd1bcf19
maniyadg <maniyarasandhanasekaran@gmail.com>
2022-12-15 12:40:39 +0530

    initial

---
+++ index.js
@@ -0,0 +1,173 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const uniqid = require("uniqid");
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+
+console.log(uniqid(), uniqid());
+const rooms = [];
+const bookings = [];
+
+// to check api working or not
+app.get("/", function (req, res) {
+  res.json({message:"working"});
+});
+
+
+// to get all rooms
+app.get("/getAllRooms", function (req, res) {
+  res.json(rooms);
+});
+
+
+// to get all rooms with booking details
+app.get("/getAllBookings", function (req, res) {
+  res.json(bookings);
+});
+
+
+// to create a room
+app.post("/createRoom", function (req, res) {
+  let room = {};
+  room.id = uniqid();
+  room.bookings = [];
+  if (req.body.noSeats) {
+    room.noSeats = req.body.noSeats;
+  } else {
+    res.status(400).json({ output: "Please specify No of seats for Room" });
+  }
+  if (req.body.amenities) {
+    room.amenities = req.body.amenities;
+  } else {
+    res
+      .status(400)
+      .json({
+        output: "Please specify all Amenities for Room in Array format",
+      });
+  }
+  if (req.body.price) {
+    room.price = req.body.price;
+  } else {
+    res.status(400).json({ output: "Please specify price per hour for Room" });
+  }
+  rooms.push(room);
+
+  res.status(200).json({ output: "Room Created Successfully" });
+});
+
+
+// to booking a room
+app.post("/createBooking", function (req, res) {
+  let booking = {};
+  booking.id = uniqid();
+  if (req.body.custName) {
+    booking.custName = req.body.custName;
+  } else {
+    res
+      .status(400)
+      .json({ output: "Please specify customer Name for booking." });
+  }
+  if (req.body.date) {
+    if (req.body.date) {
+      booking.date = req.body.date;
+    } else {
+      res.status(400).json({ output: "Please specify date in MM/DD/YYYY" });
+    }
+  } else {
+    res.status(400).json({ output: "Please specify date for booking." });
+  }
+
+  if (req.body.startTime) {
+    if (req.body.startTime) {
+      booking.startTime = req.body.startTime;
+    } else {
+      res
+        .status(400)
+        .json({
+          output:
+            "Please specify time in hh:min(24-hr format) where minutes should be 00 only",
+        });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ output: "Please specify Starting time for booking." });
+  }
+
+  if (req.body.endTime) {
+    if (req.body.endTime) {
+      booking.endTime = req.body.endTime;
+    } else {
+      res
+        .status(400)
+        .json({
+          output:
+            "Please specify time in hh:min(24-hr format) where minutes should be 00 only",
+        });
+    }
+  } else {
+    res.status(400).json({ output: "Please specify Ending time for booking." });
+  }
+
+
+  // to check rooms are available or not
+  const availableRooms = rooms.filter((room) => {
+    if (room.bookings.length == 0) {
+      return true;
+    } else {
+      room.bookings.filter((book) => {
+        if (book.date == req.body.date) {
+          if (
+            parseInt(book.startTime.substring(0, 1)) >
+              parseInt(req.body.startTime.substring(0, 1)) &&
+            parseInt(book.startTime.substring(0, 1)) >
+              parseInt(req.body.endTime.substring(0, 1))
+          ) {
+            if (
+              parseInt(book.startTime.substring(0, 1)) <
+                parseInt(req.body.startTime.substring(0, 1)) &&
+              parseInt(book.startTime.substring(0, 1)) <
+                parseInt(req.body.endTime.substring(0, 1))
+            ) {
+              return true;
+            }
+          }
+        } else {
+          return true;
+        }
+      });
+    }
+  });
+  if (availableRooms.length == 0) {
+    res
+      .status(400)
+      .json({ output: "No Available Rooms on Selected Date and Time" });
+  } else {
+    roomRec = availableRooms[0];
+    let count = 0;
+    rooms.forEach((element) => {
+      if (element.roomNo == roomRec.roomNo) {
+        rooms[count].bookings.push({
+          custName: req.body.custName,
+          startTime: req.body.startTime,
+          endTime: req.body.endTime,
+          date: req.body.date,
+        });
+      }
+      count++;
+    });
+    let bookingRec = req.body;
+    bookingRec.roomNo = roomRec.roomNo;
+    bookingRec.cost =
+      parseInt(roomRec.price) *
+      (parseInt(bookingRec.endTime.substring(0, 1)) -
+        parseInt(bookingRec.startTime.substring(0, 1)));
+
+    bookings.push(bookingRec);
+    res.status(200).json({ output: "Room Booking Successfully" });
+  }
+});
+
+
+app.listen(3000)
